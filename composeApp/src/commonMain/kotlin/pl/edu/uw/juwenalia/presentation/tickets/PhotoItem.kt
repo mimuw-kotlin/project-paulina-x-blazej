ea9c781d0d4366df93c94567ee3ef2d5a13943f6
Błażej Wilkoławski <blazej2801@hotmail.com>
2025-01-29 16:49:06 +0100

    Tickets screen cleanup, implemented bottom sheet

--- composeApp/src/commonMain/kotlin/pl/edu/uw/juwenalia/presentation/tickets/PhotoItem.kt
+++ composeApp/src/commonMain/kotlin/pl/edu/uw/juwenalia/presentation/tickets/PhotoItem.kt
@@ -1,90 +1,62 @@
 package pl.edu.uw.juwenalia.presentation.tickets
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import io.github.vinceglb.filekit.core.PlatformFile
 
 @Composable
-fun PhotoItem(
-    file: PlatformFile
-) {
+internal fun PhotoItem(file: PlatformFile) {
     var bytes by remember(file) { mutableStateOf<ByteArray?>(null) }
-    var showName by remember { mutableStateOf(false) }
 
     LaunchedEffect(file) {
-        bytes = if (file.supportsStreams()) {
-            val size = file.getSize()
-            if (size != null && size > 0L) {
-                val buffer = ByteArray(size.toInt())
-                val tmpBuffer = ByteArray(1000)
-                var totalBytesRead = 0
-                file.getStream().use {
-                    while (it.hasBytesAvailable()) {
-                        val numRead = it.readInto(tmpBuffer, 1000)
-                        tmpBuffer.copyInto(
-                            buffer,
-                            destinationOffset = totalBytesRead,
-                            endIndex = numRead,
-                        )
-                        totalBytesRead += numRead
+        bytes =
+            if (file.supportsStreams()) {
+                val size = file.getSize()
+                if (size != null && size > 0L) {
+                    val buffer = ByteArray(size.toInt())
+                    val tmpBuffer = ByteArray(1000)
+                    var totalBytesRead = 0
+                    file.getStream().use {
+                        while (it.hasBytesAvailable()) {
+                            val numRead = it.readInto(tmpBuffer, 1000)
+                            tmpBuffer.copyInto(
+                                buffer,
+                                destinationOffset = totalBytesRead,
+                                endIndex = numRead
+                            )
+                            totalBytesRead += numRead
+                        }
                     }
+                    buffer
+                } else {
+                    file.readBytes()
                 }
-                buffer
             } else {
                 file.readBytes()
             }
-        } else {
-            file.readBytes()
-        }
     }
 
-    Surface(
-        onClick = { showName = !showName },
-        modifier = Modifier
-            .clip(shape = MaterialTheme.shapes.medium)
-    ) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            bytes?.let {
-                AsyncImage(
-                    bytes,
-                    contentDescription = file.name,
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier.fillMaxSize()
-                )
-            }
-
-            AnimatedVisibility(
-                visible = showName,
-                modifier = Modifier.padding(4.dp).align(Alignment.BottomStart)
-            ) {
-                Surface(
-                    color = MaterialTheme.colorScheme.surface.copy(alpha = 0.7f),
-                    shape = MaterialTheme.shapes.small,
-                ) {
-                    Text(
-                        file.name,
-                        style = MaterialTheme.typography.labelMedium,
-                        modifier = Modifier.padding(4.dp)
-                    )
-                }
-            }
-        }
+    bytes?.let {
+        AsyncImage(
+            bytes,
+            contentDescription = file.name,
+            contentScale = ContentScale.Fit,
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(16.dp)
+        )
     }
 }
--- composeApp/src/commonMain/kotlin/pl/edu/uw/juwenalia/presentation/tickets/TicketsScreen.kt
+++ composeApp/src/commonMain/kotlin/pl/edu/uw/juwenalia/presentation/tickets/TicketsScreen.kt
@@ -18,8 +18,10 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -33,7 +35,10 @@ import io.github.vinceglb.filekit.core.PickerType
 import io.github.vinceglb.filekit.core.PlatformDirectory
 import io.github.vinceglb.filekit.core.PlatformFile
 import juweappka.composeapp.generated.resources.Res
+import juweappka.composeapp.generated.resources.delete
+import juweappka.composeapp.generated.resources.ticket_image_content_desc
 import juweappka.composeapp.generated.resources.ticket_image_placeholder
+import juweappka.composeapp.generated.resources.ticket_item_title
 import juweappka.composeapp.generated.resources.tickets_title
 import juweappka.composeapp.generated.resources.upload_ticket
 import juweappka.composeapp.generated.resources.your_tickets
@@ -42,103 +47,116 @@ import pl.edu.uw.juwenalia.presentation.components.CardGridItem
 import pl.edu.uw.juwenalia.presentation.components.CardWithAction
 import pl.edu.uw.juwenalia.presentation.components.FeedSectionHeader
 
-sealed class TicketsDestinations {
-    object TicketScreen : TicketsDestinations()
-    object TicketsViewScreen : TicketsDestinations()
-}
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun TicketsScreen() {
     val uriHandler = LocalUriHandler.current
-    var showFilePicker by remember { mutableStateOf(false) }
+
+    val sheetState = rememberModalBottomSheetState()
+    var showBottomSheet by remember { mutableStateOf(false) }
 
     var files: Set<PlatformFile> by remember { mutableStateOf(emptySet()) }
     var selectedFile: PlatformFile? by remember { mutableStateOf(null) }
-    var directory: PlatformDirectory? by remember { mutableStateOf(null) }
+    val directory: PlatformDirectory? by remember { mutableStateOf(null) }
 
-    var currentDestination by remember {
-        mutableStateOf<TicketsDestinations>(TicketsDestinations.TicketScreen) }
+    val ticketFilePicker =
+        rememberFilePickerLauncher(
+            type = PickerType.File(listOf("png")),
+            initialDirectory = directory?.path,
+            onResult = { file -> file?.let { files += it } }
+        )
 
-    val ticketFilePicker = rememberFilePickerLauncher(type = PickerType.File(listOf("png")),
-        initialDirectory = directory?.path,
-        onResult = { file -> file?.let { files += it } })
-
-    when (currentDestination) {
-        is TicketsDestinations.TicketScreen -> {
-            Scaffold(topBar = {
-                CenterAlignedTopAppBar(
-                    title = { Text(text = stringResource(Res.string.tickets_title)) })
-            }, floatingActionButton = {
-                ExtendedFloatingActionButton(onClick = { ticketFilePicker.launch() },
-                    icon = { Icon(Icons.Filled.Upload, stringResource(Res.string.upload_ticket)) },
-                    text = { Text(text = stringResource(Res.string.upload_ticket)) })
-            }) { innerPadding ->
-                LazyVerticalGrid(
-                    columns = GridCells.Adaptive(minSize = 180.dp),
-                    contentPadding = if (files.isEmpty()) {
-                        PaddingValues(horizontal = 16.dp)
-                    } else {
-                        PaddingValues(start = 16.dp, end = 16.dp, bottom = 120.dp)
-                    },
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    verticalArrangement = Arrangement.spacedBy(8.dp),
-                    modifier = Modifier.consumeWindowInsets(innerPadding).padding(innerPadding)
-                ) {
-                    item(span = { GridItemSpan(maxLineSpan) }) {
-                        CardWithAction(modifier = Modifier.fillMaxWidth().wrapContentHeight(),
-                            title = "Pierwsza pula biletów",
-                            subtitle = "Już dostępna!",
-                            bodyText = "Nie zwlekaj – liczba biletów w puli jest limitowana" +
-                                        ", a ceny będą wzrastać.",
-                            buttonIcon = Icons.Filled.ShoppingCart,
-                            buttonText = "Kup teraz",
-                            onButtonClick = {
-                                uriHandler.openUri("https://www.mimuw.edu.pl/pl/")
-                            })
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(
+            title = { Text(text = stringResource(Res.string.tickets_title)) }
+        )
+    }, floatingActionButton = {
+        ExtendedFloatingActionButton(
+            onClick = { ticketFilePicker.launch() },
+            icon = { Icon(Icons.Filled.Upload, stringResource(Res.string.upload_ticket)) },
+            text = { Text(text = stringResource(Res.string.upload_ticket)) }
+        )
+    }) { innerPadding ->
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 180.dp),
+            contentPadding =
+                if (files.isEmpty()) {
+                    PaddingValues(horizontal = 16.dp)
+                } else {
+                    PaddingValues(start = 16.dp, end = 16.dp, bottom = 120.dp)
+                },
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.consumeWindowInsets(innerPadding).padding(innerPadding)
+        ) {
+            item(span = { GridItemSpan(maxLineSpan) }) {
+                CardWithAction(
+                    modifier = Modifier.fillMaxWidth().wrapContentHeight(),
+                    title = "Pierwsza pula biletów",
+                    subtitle = "Już dostępna!",
+                    bodyText =
+                        "Nie zwlekaj – liczba biletów w puli jest limitowana" +
+                            ", a ceny będą wzrastać.",
+                    buttonIcon = Icons.Filled.ShoppingCart,
+                    buttonText = "Kup teraz",
+                    onButtonClick = {
+                        uriHandler.openUri("https://www.mimuw.edu.pl/pl/")
                     }
-
-                    if (files.isEmpty()) {
-                        item(span = { GridItemSpan(maxLineSpan) }) {
-                            NoTicketsEmptyState(
-                                modifier = Modifier.fillMaxWidth().wrapContentHeight()
-                                    .padding(16.dp)
-                            )
-                        }
-                    } else {
-                        item(span = { GridItemSpan(maxLineSpan) }) {
-                            FeedSectionHeader(stringResource(Res.string.your_tickets))
-                        }
-
-                        items(files.toList()) {
-
-                            val fileName: String = it.name
-                            val filePath: String? = it.path
-
-                            CardGridItem(title = "Bilet",
-                                subtitle = "$fileName",
-                                image = Res.drawable.ticket_image_placeholder,
-                                imageContentDescription = "Tickets",
-                                buttonIcon = Icons.Filled.Delete,
-                                buttonIconContentDescription = "Usuń",
-                                onCardClick = {
-                                    currentDestination = TicketsDestinations.TicketsViewScreen
-                                    selectedFile = it
-                                },
-                                onButtonClick = {
-                                    files = files.filterNot { it.path == filePath }.toSet()
-                                })
-                        }
-                    }
-                }
+                )
             }
-        }
 
-        is TicketsDestinations.TicketsViewScreen -> {
-            selectedFile?.let {
-                TicketsViewScreen(ticket = it)
+            if (files.isEmpty()) {
+                item(span = { GridItemSpan(maxLineSpan) }) {
+                    NoTicketsEmptyState(
+                        modifier =
+                            Modifier
+                                .fillMaxWidth()
+                                .wrapContentHeight()
+                                .padding(16.dp)
+                    )
+                }
+            } else {
+                item(span = { GridItemSpan(maxLineSpan) }) {
+                    FeedSectionHeader(stringResource(Res.string.your_tickets))
+                }
+
+                items(files.toList()) { file ->
+                    val fileName: String = file.name
+                    val filePath: String? = file.path
+
+                    CardGridItem(
+                        title = stringResource(Res.string.ticket_item_title),
+                        subtitle = fileName,
+                        image = Res.drawable.ticket_image_placeholder,
+                        imageContentDescription =
+                            stringResource(
+                                Res.string.ticket_image_content_desc
+                            ),
+                        buttonIcon = Icons.Filled.Delete,
+                        buttonIconContentDescription = stringResource(Res.string.delete),
+                        onCardClick = {
+                            selectedFile = file
+                            showBottomSheet = true
+                        },
+                        onButtonClick = {
+                            files = files.filterNot { it.path == filePath }.toSet()
+                        }
+                    )
+                }
             }
         }
     }
 
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                showBottomSheet = false
+            },
+            sheetState = sheetState
+        ) {
+            selectedFile?.let {
+                PhotoItem(file = it)
+            }
+        }
+    }
 }
