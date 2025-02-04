b590057c006b25dd2b0aff8fd3cd7b0f00e95174
paulina <paulinakubera888@gmail.com>
2025-02-04 17:31:51 +0100

    Artist model added

--- composeApp/src/commonMain/kotlin/pl/edu/uw/juwenalia/data/model/Artist.kt
+++ composeApp/src/commonMain/kotlin/pl/edu/uw/juwenalia/data/model/Artist.kt
@@ -1,9 +1,8 @@
 package pl.edu.uw.juwenalia.data.model
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 data class Artist(
+    val id: Int,
     val name: String,
-    val imageFilename: String
+    val imageFilename: String,
+    val imageByteArray: ByteArray
 )
